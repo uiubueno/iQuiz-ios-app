169c5aa8ace5ced58e3a9944ff9f7302898e2459
William Bueno <165913163+uiubueno@users.noreply.github.com>
2025-03-02 18:35:17 -0300

    Add files via upload
    Utilizado de constraints, adicionado elementos do storyboard no código e ações, realizado navegação entre telas e criado uma nova viewcontroller

---
+++ iQuiz/QuestaoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  QuestaoViewController.swift
+//  iQuiz
+//
+//  Created by William Bueno on 02/03/25.
+//
+
+import UIKit
+
+class QuestaoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var botaoIniciarQuiz: UIButton!
+    
+    @IBAction func BotaoPressionado(_ sender: Any) {
+        print("O botão foi pressionado!")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configuraLayout()
     }
-
-
+    func configuraLayout() {
+        botaoIniciarQuiz.layer.cornerRadius = 12.0
+        botaoIniciarQuiz.backgroundColor = UIColor(red: 116/255, green: 50/255, blue: 255/255, alpha: 1.0)
+    }
+    
 }
 
